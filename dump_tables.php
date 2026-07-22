604de5a28dc627e9e772ba4759a6ab7e50b6d861
yashhagare7 <yashhagare7@gmail.com>
2026-07-22 14:31:48 +0530

    Deleted Unnessary Files

--- dump_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $db = new PDO('sqlite:database.sqlite');
-    $stmt = $db->query("SELECT DISTINCT status FROM attendance");
-    print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-} catch(Exception $e) {
-    echo $e->getMessage();
-}
--- fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let s = fs.readFileSync('public/script.js', 'utf8');
-s = s.replace(/\\`/g, '`').replace(/\\\$/g, '$');
-fs.writeFileSync('public/script.js', s);
-console.log('Fixed script.js');
